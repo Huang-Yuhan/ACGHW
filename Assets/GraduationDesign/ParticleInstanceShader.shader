c9eb0e074b06a9bd0d1f23ba1cb84bb33a9c16fb
IcecreamSarkaz <529497790@qq.com>
2025-03-03 19:01:16 +0800

    update

---
+++ Assets/GraduationDesign/ParticleInstanceShader.shader
@@ -0,0 +1,49 @@
+Shader "Custom/ParticleInstanceShader"
+{
+    SubShader
+    {
+        
+        Pass
+        {
+            
+            
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+            #define UNITY_INDIRECT_DRAW_ARGS IndirectDrawIndexedArgs
+            #include "UnityIndirect.cginc"
+
+            struct v2f
+            {
+                float4 pos : SV_POSITION;
+                float4 color : COLOR0;
+            };
+
+
+
+            StructuredBuffer<float3> _PositionBuffer;
+            float _ParticleRadius;
+            
+            v2f vert(appdata_base v, uint svInstanceID : SV_InstanceID)
+            {
+                InitIndirectDrawArgs(0);
+                v2f o;
+                uint cmdID = GetCommandID(0);
+                uint instanceID = GetIndirectInstanceID(svInstanceID);
+                float3 center = _PositionBuffer[instanceID];
+                float4 wpos = float4(v.vertex.xyz*_ParticleRadius + center, 1);
+                o.pos = UnityObjectToClipPos(wpos);
+                o.color = float4(1, 1, 1, 1);
+                return o;
+            }
+
+            float4 frag(v2f i) : SV_Target
+            {
+                return i.color;
+            }
+            ENDCG
+        }
+    }
+}
--- Assets/GraduationDesign/PlaneRegister.cs
+++ Assets/GraduationDesign/PlaneRegister.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace GraduationDesign
@@ -24,7 +25,14 @@ namespace GraduationDesign
         
         public static List<plane_data_type> plane_data = new List<plane_data_type>();
         public float particle_radius = 0.1f;
-        
+        public bool isRenderParticle = false;
+        public Material material;
+        public Mesh mesh;
+        private GraphicsBuffer commandBuf;
+        private GraphicsBuffer.IndirectDrawIndexedArgs[] commandData;
+        private RenderParams rp;
+        private ComputeBuffer particelPositionBuffer;
+        private plane_data_type thisOneplaneData;
         public static void RegisterPlane(Vector3 position, Vector3 normal, Vector3 a_axis, Vector3 b_axis, float particle_radius)
         {
             plane_data_type plane_data_temp = new plane_data_type();
@@ -34,6 +42,7 @@ namespace GraduationDesign
             plane_data_temp.b_axis = b_axis;
             plane_data_temp.particle_radius = particle_radius;
             plane_data.Add(plane_data_temp);
+            
             Debug.LogFormat("Register Plane: {0}, {1}, {2}, {3}, {4}", position, normal, a_axis, b_axis, particle_radius);
         }
 
@@ -49,6 +58,65 @@ namespace GraduationDesign
             Vector3 a_axis=a_axis_in_unity*transform.localScale.x;
             Vector3 b_axis=b_axis_in_unity*transform.localScale.z;
             RegisterPlane(origin, normal, a_axis, b_axis, particle_radius);
+            
+            thisOneplaneData = plane_data[plane_data.Count - 1];
+            
+        }
+
+        void RenderParticle()
+        {
+            material.SetBuffer("_PositionBuffer", particelPositionBuffer);
+            material.SetFloat("_ParticleRadius", particle_radius);
+            Graphics.RenderMeshIndirect(rp, mesh, commandBuf, 1);
+        }
+
+
+        
+        private void Start()
+        {
+            uint x_max_count = (uint)(thisOneplaneData.a_axis.magnitude / particle_radius/2);
+            uint z_max_count = (uint)(thisOneplaneData.b_axis.magnitude / particle_radius/2);
+            Debug.LogFormat("x_max_count: {0}, z_max_count: {1}", x_max_count, z_max_count);
+            commandBuf = new GraphicsBuffer(GraphicsBuffer.Target.IndirectArguments, 1, GraphicsBuffer.IndirectDrawIndexedArgs.size);
+            commandData = new GraphicsBuffer.IndirectDrawIndexedArgs[1];
+            rp = new RenderParams(material);
+            rp.worldBounds = new Bounds(Vector3.zero, 100*Vector3.one); // use tighter bounds for better FOV culling
+            commandData[0].indexCountPerInstance = mesh.GetIndexCount(0);
+            commandData[0].instanceCount = x_max_count*z_max_count;
+            commandBuf.SetData(commandData);
+            particelPositionBuffer = new ComputeBuffer((int)(x_max_count * z_max_count), sizeof(float) * 3);
+            Vector3[] data = new Vector3[x_max_count*z_max_count];
+            for (int i = 0; i < x_max_count; i++)
+            {
+                for (int j = 0; j < z_max_count; j++)
+                {
+                    data[i * z_max_count + j] = thisOneplaneData.position +
+                                                thisOneplaneData.a_axis.normalized * i * particle_radius * 2 +
+                                                thisOneplaneData.b_axis.normalized * j * particle_radius * 2;
+                }
+            }
+            particelPositionBuffer.SetData(data);
+            
+        }
+
+        private void Update()
+        {
+            if (isRenderParticle)
+            {
+                GetComponent<MeshRenderer>().enabled = false;
+                RenderParticle();
+            }else
+            {
+                GetComponent<MeshRenderer>().enabled = true;
+            }
+        }
+        
+        void OnDestroy()
+        {
+            commandBuf?.Release();
+            commandBuf = null;
+            particelPositionBuffer?.Release();
+            particelPositionBuffer = null;
         }
     }
 }
